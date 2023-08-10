1b0ba72eb70165a8729e2980f807117f84387649
Shahabas Mohammed <shahabasg1@gmail.com>
2023-08-10 21:31:41 +0530

    last part

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import './Components/Navbar/Navbar.css';
 import RowPost from './Components/RowPost/RowPost';
 import Banner from './Components/Banner/Banner';
+import { originals,actions,horror,comedy,romance,documentaries } from './url';
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Banner/>
-     <RowPost/>
+     <RowPost title='Netflix Originals' url={originals}/>
+     <RowPost title='Action' isSmall url={actions}/>
+     <RowPost title='Horror' isSmall url={horror}/>
+     <RowPost title='Comedy' isSmall url={comedy}/>
+     <RowPost title='Romance' isSmall url={romance}/>
+     <RowPost title='Documentaries' isSmall url={documentaries}/>
     </div>
   );
 }
--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -1,20 +1,59 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import './RowPost.css'
-function RowPost() {
-  return (
-    <div className='row'>
-         <h2>Title</h2>
-         <div className='posters'>
-            <img className='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="poster" />
-            <img className='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="poster" />
-            <img className='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="poster" />
-            <img className='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="poster" />
-            <img className='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="poster" />
-            <img className='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="poster" />
-            <img className='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="poster" />
-            <img className='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' alt="poster" />
+import axios from '../../axios'
+import Youtube from 'react-youtube'
+import { imageUrl } from '../../constants/constants'
+import { API_KEY } from '../../constants/constants'
+function RowPost(props) {
+ const [movies, setMovies] = useState([])
+  useEffect(()=>{
+    axios.get(props.url).then((response)=>{
+      // console.log(response.data)
+      setMovies(response.data.results)
+    }).catch(error=>{
+      console.log('err')
+    })
+  },[])
+const [urlId,setUrlId]=useState('')
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+   const [trailerScreen, setTrailerScreen] = useState(false)
+  const handleMovie=(id)=>{
+ axios.get(`movie/${id}/videos?api_key=${API_KEY}`).then((response)=>{
+  if(response.data.results.length !== 0){
+     setUrlId(response.data.results[0])
+     setTrailerScreen(true)
+  }else{
 
+  }
+ })
+  }
+  return (
+
+    <div className='row'>
+         <h2>{props.title}</h2>
+         <div className='posters'>
+      {
+        movies.map((obj)=>(
+
+          <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ?'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="poster" />
+        ))
+      }
+           
          </div>
+       { trailerScreen &&  (
+              <div className="overlay">
+                <div style={{width:'80%'}}>
+                  <Youtube opts={opts} videoId={urlId.key}/>
+                  <button className='close-button' onClick={()=>{setTrailerScreen(false)}}>Close</button>
+                </div>
+              </div>)} 
     </div>
   )
 }
